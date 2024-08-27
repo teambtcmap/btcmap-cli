587b10359e6ff5ff42b2412b27276e55ca04e1dd
Igor Bubelov <igor@bubelov.com>
2024-08-27 13:58:41 +0700

    Save user token

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,64 @@
+use dirs::data_dir;
+use rusqlite::params;
+use rusqlite::Connection;
 use serde_json::{json, Map, Value};
-use std::env;
+use std::{env, fs::create_dir};
 
 fn main() {
-    let token = env::var("TOKEN").unwrap();
+    let data_dir = data_dir().unwrap().join("mapctl");
+    println!("Data dir: {}", data_dir.to_str().unwrap());
+
+    if !data_dir.exists() {
+        print!("Data dir did not exist, creating...");
+        create_dir(&data_dir).unwrap();
+    }
+
+    let conn = Connection::open(data_dir.join("mapctl.db")).unwrap();
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS settings (json TEXT NOT NULL);",
+        (), // empty list of parameters.
+    )
+    .unwrap();
+
+    let mut stmt = conn.prepare("SELECT COUNT (*) FROM settings;").unwrap();
+    let mut rows = stmt.query(()).unwrap();
+    let first_row = rows.next().unwrap().unwrap();
+    let rows: i64 = first_row.get(0).unwrap();
+
+    if rows == 0 {
+        conn.execute("INSERT INTO settings (json) VALUES (json('{}'))", ())
+            .unwrap();
+    }
+
+    let mut stmt = conn
+        .prepare("SELECT json_extract(json, '$.token') FROM settings;")
+        .unwrap();
+    let mut rows = stmt.query(()).unwrap();
+
+    let token: String = match rows.next().unwrap() {
+        Some(first_row) => first_row.get(0).unwrap_or("".into()),
+        None => "".into(),
+    };
+
     let args: Vec<String> = env::args().collect();
-    match args[1].as_str() {
+    let command = args[1].as_str();
+
+    if token.is_empty() && command != "set-token" {
+        println!("You're not autorized. Please run mapctl set-token <token>");
+        return;
+    }
+
+    match command {
+        "set-token" => {
+            let token = args[2].clone();
+            conn.execute(
+                "UPDATE settings SET json = json_set(json, '$.token', ?1);",
+                params![token],
+            )
+            .unwrap();
+            println!("Saved {token} as a token for all future calls");
+        }
         "get-area" => {
             let id_or_alias = args[2].clone();
             let client = reqwest::blocking::Client::new();
