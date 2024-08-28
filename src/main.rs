e1b71c1f64a2c3e936a27eb0c03e55cf1e01dbd6
Igor Bubelov <igor@bubelov.com>
2024-08-28 15:43:31 +0700

    Add get-element command

--- src/main.rs
+++ src/main.rs
@@ -60,10 +60,10 @@ fn main() {
             println!("Saved {token} as a token for all future calls");
         }
         "get-area" => {
-            let id_or_alias = args[2].clone();
+            let id = args[2].clone();
             let client = reqwest::blocking::Client::new();
             let args = json!(
-                {"jsonrpc": "2.0", "method": "getarea", "params": {"token": token, "area_id_or_alias": id_or_alias}, "id": 1}
+                {"jsonrpc": "2.0", "method": "getarea", "params": {"token": token, "id": id}, "id": 1}
             );
             println!("{args}");
             let mut res = client
@@ -85,14 +85,14 @@ fn main() {
             println!("{}", res);
         }
         "set-area-tag" => {
-            let id_or_alias = args[2].clone();
-            let tag_name = args[3].clone();
-            let tag_value = args[4].clone();
-            println!("{}", tag_value);
-            let tag_value: Value = serde_json::from_str(&tag_value).unwrap();
+            let id = args[2].clone();
+            let name = args[3].clone();
+            let value = args[4].clone();
+            println!("{}", value);
+            let value: Value = serde_json::from_str(&value).unwrap();
             let client = reqwest::blocking::Client::new();
             let args = json!(
-                {"jsonrpc": "2.0", "method": "setareatag", "params": {"token": token, "area_id_or_alias": id_or_alias, "tag_name": tag_name, "tag_value": tag_value}, "id": 1}
+                {"jsonrpc": "2.0", "method": "setareatag", "params": {"token": token, "id": id, "name": name, "value": value}, "id": 1}
             );
             println!("{args}");
             let mut res = client
@@ -114,11 +114,11 @@ fn main() {
             println!("{}", res);
         }
         "remove-area-tag" => {
-            let id_or_alias = args[2].clone();
-            let tag_name = args[3].clone();
+            let id = args[2].clone();
+            let tag = args[3].clone();
             let client = reqwest::blocking::Client::new();
             let args = json!(
-                {"jsonrpc": "2.0", "method": "removeareatag", "params": {"token": token, "area_id_or_alias": id_or_alias, "tag_name": tag_name}, "id": 1}
+                {"jsonrpc": "2.0", "method": "removeareatag", "params": {"token": token, "id": id, "tag": tag}, "id": 1}
             );
             println!("{args}");
             let mut res = client
@@ -136,6 +136,26 @@ fn main() {
                 .as_object_mut()
                 .unwrap()
                 .remove("geo_json");
+            let res = serde_json::to_string_pretty(&res).unwrap();
+            println!("{}", res);
+        }
+        "get-element" => {
+            let id = args[2].clone();
+            let client = reqwest::blocking::Client::new();
+            let args = json!(
+                {"jsonrpc": "2.0", "method": "getelement", "params": {"token": token, "id": id}, "id": 1}
+            );
+            println!("{args}");
+            let res = client
+                .post("https://api.btcmap.org/rpc")
+                .body(serde_json::to_string(&args).unwrap())
+                .send()
+                .unwrap()
+                .json::<Map<String, Value>>()
+                .unwrap()
+                .get("result")
+                .unwrap()
+                .clone();
             let res = serde_json::to_string_pretty(&res).unwrap();
             println!("{}", res);
         }
