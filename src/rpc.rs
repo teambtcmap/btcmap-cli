88ae103d73814db9ba9077d26bb8b92fd63eac33
Igor Bubelov <igor@bubelov.com>
2024-10-07 22:26:04 +0700

    Improve error handling

--- src/rpc.rs
+++ src/rpc.rs
@@ -13,41 +13,20 @@ pub fn call_remote_procedure(name: &str, mut params: Value) -> Result<()> {
         "password".into(),
         db::query_settings_string("password", &db::connect()?)?.into(),
     );
-    let http_client = ClientBuilder::new().timeout(None).build().map_err(|e| {
-        eprintln!("failed to initialize HTTP client: {e}");
-        e
-    })?;
+    let http_client = ClientBuilder::new().timeout(None).build()?;
     let args = json!(
         {"jsonrpc": "2.0", "method": name, "params": params, "id": 1}
     );
-    let args = serde_json::to_string(&args).map_err(|e| {
-        eprintln!("failed to convert args to string: {e}");
-        e
-    })?;
+    let args = serde_json::to_string(&args)?;
     let mut api_url = db::query_settings_string("api_url", &db::connect()?)?;
     if api_url.trim().is_empty() {
         api_url = "https://api.btcmap.org/rpc".into();
     }
-    let res = http_client.post(api_url).body(args).send().map_err(|e| {
-        eprintln!("request failed: {e}");
-        e
-    })?;
+    let res = http_client.post(api_url).body(args).send()?;
     if res.status().is_success() {
-        let res = res.json::<Map<String, Value>>().map_err(|e| {
-            eprintln!("failed to convert response body to JSON object: {e}");
-            e
-        })?;
-        let res = serde_json::to_string_pretty(&res).map_err(|e| {
-            eprintln!("failed to convert JSON object to string: {e}");
-            e
-        })?;
-        println!(
-            "{}",
-            res.to_colored_json_auto().map_err(|e| {
-                eprintln!("failed to create colored JSON: {e}");
-                e
-            })?
-        );
+        let res = res.json::<Map<String, Value>>()?;
+        let res = serde_json::to_string_pretty(&res)?;
+        println!("{}", res.to_colored_json_auto()?);
     } else {
         handle_unsuccessful_response(res);
     }
