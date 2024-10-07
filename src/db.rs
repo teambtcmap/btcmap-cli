d6799ade70e42ec1f2c0e4374613255754cf0186
Igor Bubelov <igor@bubelov.com>
2024-10-07 21:21:44 +0700

    Improve error handling

--- src/db.rs
+++ src/db.rs
@@ -13,46 +13,27 @@ pub fn insert_settings_string(name: &str, value: &str, conn: &Connection) -> Res
     conn.execute(
         &format!("UPDATE settings SET json = json_set(json, '$.{name}', ?1);"),
         params![value],
-    )
-    .map_err(|e| {
-        eprintln!("failed to set {name} to {value}: {e}");
-        e
-    })?;
+    )?;
     Ok(())
 }
 
 pub fn query_settings_string(name: &str, conn: &Connection) -> Result<String> {
-    let mut stmt = conn
-        .prepare(&format!(
-            "SELECT json_extract(json, '$.{name}') FROM settings;"
-        ))
-        .map_err(|e| {
-            eprintln!("failed to query {name} from settings");
-            e
-        })?;
-    let mut rows = stmt.query(()).map_err(|e| {
-        eprintln!("failed to query {name} from settings");
-        e
-    })?;
-    Ok(
-        match rows.next().map_err(|e| {
-            eprintln!("failed to query {name} from settings");
-            e
-        })? {
-            Some(first_row) => first_row.get(0).unwrap_or("".into()),
-            None => "".into(),
-        },
-    )
+    let mut stmt = conn.prepare(&format!(
+        "SELECT json_extract(json, '$.{name}') FROM settings;"
+    ))?;
+    let mut rows = stmt.query(())?;
+    let res = match rows.next()? {
+        Some(first_row) => first_row.get(0).unwrap_or("".into()),
+        None => "".into(),
+    };
+    Ok(res)
 }
 
 fn path() -> Result<PathBuf> {
     let data_dir = data_dir().ok_or("failed to locate system app data directory")?;
     let data_dir = data_dir.join("btcmap-cli");
     if !data_dir.exists() {
-        create_dir(&data_dir).map_err(|e| {
-            eprintln!("failed to create database directory");
-            e
-        })?;
+        create_dir(&data_dir)?;
     }
     Ok(data_dir.join("btcmap-cli.db"))
 }
@@ -61,38 +42,13 @@ fn init(conn: &Connection) -> Result<()> {
     conn.execute(
         "CREATE TABLE IF NOT EXISTS settings (json TEXT NOT NULL);",
         (),
-    )
-    .map_err(|e| {
-        eprintln!("failed to create settings table: {e}");
-        e
-    })?;
-    let mut stmt = conn
-        .prepare("SELECT COUNT (*) FROM settings;")
-        .map_err(|e| {
-            eprintln!("failed to query settings table: {e}");
-            e
-        })?;
-    let mut rows = stmt.query(()).map_err(|e| {
-        eprintln!("failed to query settings table: {e}");
-        e
-    })?;
-    let first_row = rows
-        .next()
-        .map_err(|e| {
-            eprintln!("failed to query settings table: {e}");
-            e
-        })?
-        .ok_or("COUNT query returned no rows")?;
-    let rows: i64 = first_row.get(0).map_err(|e| {
-        eprintln!("failed to query settings table {e}");
-        e
-    })?;
+    )?;
+    let mut stmt = conn.prepare("SELECT COUNT (*) FROM settings;")?;
+    let mut rows = stmt.query(())?;
+    let first_row = rows.next()?.ok_or("COUNT query returned no rows")?;
+    let rows: i64 = first_row.get(0)?;
     if rows == 0 {
-        conn.execute("INSERT INTO settings (json) VALUES (json('{}'))", ())
-            .map_err(|e| {
-                eprintln!("Failed to initialize settings table: {e}");
-                e
-            })?;
+        conn.execute("INSERT INTO settings (json) VALUES (json('{}'))", ())?;
     }
     if rows > 1 {
         Err("settings database has been corrupted")?;
